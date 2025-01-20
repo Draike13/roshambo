43345160c0c768839abacb815581e0f9619c7a48
Draike13 <tmbiri91@gmail.com>
2025-01-20 10:46:41 -0600

    simple roshambo, 10 min app

---
+++ roshambo.js
@@ -0,0 +1,31 @@
+let computerChoice;
+
+// common random number function simplified
+function computerNumber() {
+  return (computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1);
+}
+//mega function for roshambo
+function result(choice) {
+  if (choice == 1 && computerChoice == 1) {
+    alert('draw');
+  } else if (choice == 1 && computerChoice == 2) {
+    alert('you lose');
+  } else if (choice == 1 && computerChoice == 3) {
+    alert('you win');
+  } else if (choice == 2 && computerChoice == 1) {
+    alert('you win');
+  } else if (choice == 2 && computerChoice == 2) {
+    alert('draw');
+  } else if (choice == 2 && computerChoice == 3) {
+    alert('you lose');
+  } else if (choice == 3 && computerChoice == 1) {
+    alert('you win');
+  } else if (choice == 3 && computerChoice == 2) {
+    alert('you lose');
+  } else if (choice == 3 && computerChoice == 3) {
+    alert('draw');
+  } else {
+    alert('no choice selected');
+  }
+  console.log(`you chose ${choice} and  computer chose ${computerChoice}`);
+}
