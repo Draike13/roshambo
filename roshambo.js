93cc363c16bb8cc4e8e1ad775c3878cf46c84d08
Draike13 <tmbiri91@gmail.com>
2025-01-20 11:51:20 -0600

    simplify result function

--- roshambo.js
+++ roshambo.js
@@ -6,24 +6,12 @@ function computerNumber() {
 }
 //mega function for roshambo
 function result(choice) {
-  if (choice == 1 && computerChoice == 1) {
+  if (choice == computerChoice) {
     alert('draw');
-  } else if (choice == 1 && computerChoice == 2) {
+  } else if ((choice == 1 && computerChoice == 2) || (choice == 2 && computerChoice == 3) || (choice == 3 && computerChoice == 2)) {
     alert('you lose');
-  } else if (choice == 1 && computerChoice == 3) {
+  } else if ((choice == 1 && computerChoice == 3) || (choice == 2 && computerChoice == 1) || (choice == 3 && computerChoice == 1)) {
     alert('you win');
-  } else if (choice == 2 && computerChoice == 1) {
-    alert('you win');
-  } else if (choice == 2 && computerChoice == 2) {
-    alert('draw');
-  } else if (choice == 2 && computerChoice == 3) {
-    alert('you lose');
-  } else if (choice == 3 && computerChoice == 1) {
-    alert('you win');
-  } else if (choice == 3 && computerChoice == 2) {
-    alert('you lose');
-  } else if (choice == 3 && computerChoice == 3) {
-    alert('draw');
   } else {
     alert('no choice selected');
   }
