eb75f4e848d6548e4bf5a538d5e7af6119cc7bb5
Draike13 <tmbiri91@gmail.com>
2025-01-21 14:27:15 -0600

    add objects and scoreboards

--- roshambo.js
+++ roshambo.js
@@ -1,5 +1,18 @@
 let computerChoice;
 let pcHand;
+
+let scoreboard = {
+  wins: 0,
+  loses: 0,
+  draws: 0,
+};
+
+let stats = {
+  rock: 0,
+  paper: 0,
+  scissors: 0,
+};
+
 //alert response function
 
 function choice() {
@@ -19,13 +32,37 @@ function computerNumber() {
 //mega function for roshambo
 function result(choice) {
   if (choice == computerChoice) {
+    scoreboard.draws = scoreboard.draws + 1;
     alert(`Your opponent also played ${pcHand}, you DRAW.`);
   } else if ((choice == 1 && computerChoice == 2) || (choice == 2 && computerChoice == 3) || (choice == 3 && computerChoice == 2)) {
+    scoreboard.loses = scoreboard.loses + 1;
     alert(`Your opponent played ${pcHand}, you LOST!`);
   } else if ((choice == 1 && computerChoice == 3) || (choice == 2 && computerChoice == 1) || (choice == 3 && computerChoice == 1)) {
+    scoreboard.wins = scoreboard.wins + 1;
     alert(`Your opponent played ${pcHand}, You WON!`);
   } else {
     alert('no choice selected');
   }
   console.log(`you chose ${choice} and  computer chose ${computerChoice}`);
+}
+
+//scoreboard display function
+
+function showScoreboard() {
+  alert(`You currently have ${scoreboard.wins} WINS, ${scoreboard.draws} DRAWS, and ${scoreboard.loses} LOSES.`);
+}
+
+//stat functions
+
+function updateStats(selector) {
+  if (selector === 1) {
+    stats.rock = stats.rock + 1;
+  } else if (selector === 2) {
+    stats.paper = stats.paper + 1;
+  } else if (selector === 3) {
+    stats.scissors = stats.scissors + 1;
+  }
+}
+function displayStats() {
+  alert(`You've used ROCK ${stats.rock} times, PAPER ${stats.paper} times, and SCISSORS ${stats.scissors} times.`);
 }
